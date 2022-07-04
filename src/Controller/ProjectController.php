7eb0a79169a19129bb4c63736cc130d9187a06d2
Agnija <ilzena.agnija@gmail.com>
2022-07-04 10:01:10 +0200

    recover navbar, project carousel

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/project', name: 'app_project')]
+    public function index(ProjectRepository $projectRepository): Response
+    {
+        $projects = $projectRepository->findAll();
+
+        return $this->render('project/index.html.twig', [
+            'projects' => $projects,
+        ]);
+    }
+}
+
+
