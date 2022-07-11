2fc81c5290167ea84b30cfdc6a2b961657046a46
Agnija <ilzena.agnija@gmail.com>
2022-07-11 09:48:25 +0200

    stage

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ClientRepository;
-use App\Repository\ProjectRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +19,19 @@ class ClientController extends AbstractController
 
         return $this->render('client/prof.html.twig', [
             'profProjects' => $profProjects,
+         ]);
+    }
+
+    #[Route('/client/particulier', name: 'app_client_partic')]
+    public function showPartic(ClientRepository $clientRepository, CategoryRepository $categoryRepository): Response
+    {
+        $client = $clientRepository->findBy(['name' => 'particulier']);
+        // $profProjects = $projectRepository->findBy(['client' => $client]);
+        $particCategories = $categoryRepository->findBy(['category' => $category]);
+
+
+        return $this->render('client/partic.html.twig', [
+            'particCategories' => $particCategories,
          ]);
     }
 }
