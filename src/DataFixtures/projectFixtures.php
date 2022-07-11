d8a075d982ec704815aa148f327193259953b258
edetmaxence <edet.maxence@gmail.com>
2022-07-11 09:27:19 +0200

    projectFixtures

--- src/DataFixtures/projectFixtures.php
+++ src/DataFixtures/projectFixtures.php
@@ -11,7 +11,7 @@ class ProjectFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Faker\Factory::create('fr_FR');
+        $faker = Faker\Factory::create();
 
         for ($i=0; $i < 10; $i++) 
         {
